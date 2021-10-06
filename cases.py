302ecc7231d750c8afbed0692b23bb7ae0c21d98
manue <manuelcastr88@gmail.com>
2021-10-06 13:32:41 -0400

    -Vaccination data up to date -Minor vaccination label and style changes -Daily cases data and plots and added

---
+++ cases.py
@@ -0,0 +1,177 @@
+#!python3.9
+from datetime import date
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter, AutoMinorLocator, FixedLocator
+
+
+def read_data_file(filename):
+    with open(filename, encoding='utf-8') as f:
+        labels = f.readline().strip().split('\t')
+        dates, numbers = list(), list()
+        for line in f.readlines():
+            if line.strip():
+                data = line.strip().split('\t')
+                dates.append(date.fromisoformat(data[0]))
+                numbers.append(list(map(int, data[1:])))
+    return np.array(labels), np.array(dates), np.array(numbers)
+
+
+def common_style_settings(dates, n_days, y_max, y_label='', format_func=None, first_date=0,
+                          auto_y=5, legend=True, loc='upper left'):
+    x_labels = [d.strftime("%d/%m") for d in dates]
+    for i in range(n_days - 1):
+        if i % 7:
+            x_labels[n_days-1 - i] = ''
+    if first_date != 0:
+        x_labels[0] = ''
+        x_labels[first_date] = dates[first_date].strftime("%d/%m")
+        x_labels[first_date+1] = ''
+        x_labels[first_date+2] = ''
+    use_labels = np.array(x_labels)
+    no_labels = [1, 2]
+    if first_date in no_labels:
+        no_labels.remove(first_date)
+    use_labels[no_labels] = ''
+    tick_positions = np.where(x_labels)[0]
+    plt.xticks(tick_positions, use_labels[tick_positions], rotation=45)
+    plt.gca().set_xticks(range(n_days), minor=True)
+    y_minor = AutoMinorLocator(auto_y)
+    plt.gca().yaxis.set_minor_locator(y_minor)
+    if format_func:
+        plt.gca().get_yaxis().set_major_formatter(FuncFormatter(format_func))
+
+    plt.gca().set_xlim(left=0, right=n_days)
+    plt.gca().set_ylim(bottom=0, top=y_max * 1.05)
+
+    plt.xlabel('Fecha')
+    plt.ylabel(y_label, rotation=0)
+    plt.gca().yaxis.set_label_coords(-0.015, 1)
+
+    plt.gca().spines['right'].set_visible(False)
+    plt.gca().spines['top'].set_visible(False)
+
+    plt.gca().xaxis.set_tick_params(which='minor', width=.6)
+    plt.gca().xaxis.set_tick_params(which='major', pad=-2)
+
+    plt.grid(which='major', axis='y', ls=':', lw=.5, c='gray')
+    plt.grid(which='minor', axis='y', ls=':', lw=.3, c='gray')
+
+    if legend:
+        plt.legend(loc=loc, ncol=1)
+
+    plt.tight_layout()
+
+
+def plot_daily_cases(labels, dates, numbers):
+    plt.figure('Casos activos', (13, 6))
+    plt.suptitle('Casos diarios y activos')
+
+    n_days = len(dates)
+    xs = range(n_days)
+    plt.plot(xs, numbers[:, 0], label=labels[0], color='tab:orange', marker='.', lw=2)
+    plt.plot(xs, numbers[:, 1], label=labels[1], color='tab:red', lw=2)
+
+    common_style_settings(dates, n_days, numbers[:, 1].max(), '',
+                          format_func=lambda x, p: f'{x*1e-3:1.0f} mil' if x > 0 else ''),
+
+    plt.gca().spines['bottom'].set_zorder(100)
+    plt.gca().spines['left'].set_zorder(100)
+
+
+def plot_accumulated_cases(dates, numbers):
+    plt.figure('Casos acumulados', (13, 6))
+    plt.suptitle('Casos acumulados')
+
+    n_days = len(dates)
+    xs = range(n_days)
+    plt.plot(xs, numbers, color='tab:red', lw=2)
+
+    common_style_settings(dates, n_days, numbers.max(), '',
+                          format_func=lambda x, p: f'{x*1e-3:1.0f} mil' if x > 0 else '',
+                          auto_y=2, legend=False)
+
+    plt.gca().spines['bottom'].set_zorder(100)
+    plt.gca().spines['left'].set_zorder(100)
+
+
+def plot_daily_deaths(labels, dates, numbers):
+    plt.figure('Fallecidos diarios', (13, 6))
+    plt.suptitle('Fallecidos diarios y acumulado')
+
+    n_days = len(dates)
+    xs = range(n_days)
+    plt.plot(xs, numbers[:, 1], label=labels[1], color='tab:red', lw=2)
+
+    common_style_settings(dates, n_days, numbers[:, 1].max(), '',
+                          format_func=lambda x, p: f'{x*1e-3:1.0f} mil' if x > 0 else '',
+                          legend=False)
+    plt.gca().spines['bottom'].set_zorder(100)
+    plt.gca().spines['left'].set_zorder(100)
+    ax1: plt.Axes = plt.gca()
+    ax1.minorticks_off()
+    ax1.grid(False)
+
+    ax2: plt.Axes = ax1.twinx()
+    ax2.plot(xs, numbers[:, 0], label=labels[0], color='tab:orange', marker='.', lw=2)
+    ax2.set_ylim(0, numbers[:, 0].max() * 5)
+    y2_max = round(numbers[:, 0].max(), -2)
+    ax2.yaxis.set_ticks([0, y2_max])
+    ax2.yaxis.set_ticklabels(['', str(y2_max)])
+    ax2.yaxis.set_minor_locator(FixedLocator(np.linspace(0, y2_max, 6)[1:]))
+    ax2.grid(which='major', axis='y', ls=':', lw=.5, c='gray')
+    ax2.grid(which='minor', axis='y', ls=':', lw=.3, c='gray')
+    ax2.spines['top'].set_visible(False)
+
+    lines1, labels1 = ax1.get_legend_handles_labels()
+    lines2, labels2 = ax2.get_legend_handles_labels()
+    plt.legend(lines2 + lines1, labels2 + labels1, loc='upper left', ncol=1)
+
+    plt.tight_layout()
+
+
+def plot_condition_vs_actives(labels, dates, numbers):
+    daily = numbers[:, 0]
+    deaths = numbers[:, 1]
+    serious = numbers[:, 2]
+    critical = numbers[:, 3]
+
+    def percent(a, b):
+        return np.round(a / b * 100, 2)
+
+    plt.figure('Porcientos respecto a casos', (13, 6))
+    plt.suptitle('Porciento de seriedad en casos diarios')
+
+    n_days = len(dates)
+    xs = range(n_days)
+    d_percent = percent(deaths, daily)
+    plt.plot(xs, d_percent, label=labels[0], color='black', lw=2)
+    s_percent = percent(serious, daily)
+    plt.plot(xs, s_percent, label=labels[1], color='tab:orange', lw=2)
+    c_percent = percent(critical, daily)
+    plt.plot(xs, c_percent, label=labels[2], color='tab:red', lw=2)
+
+    common_style_settings(dates, n_days, max(d_percent.max(), s_percent.max(), c_percent.max()), '',
+                          format_func=lambda x, p: f'{x:1.1f} %' if x > 0 else '', auto_y=2,
+                          legend=True, loc='best')
+
+
+def plot_situation(filename, daily_labels=None, deaths_labels=None):
+    column_labels, dates, numbers = read_data_file(filename)
+    labels = column_labels[1:3] if not daily_labels else daily_labels
+    plot_daily_cases(labels, dates, numbers[:, :2])
+    plot_accumulated_cases(dates, numbers[:, 2])
+    labels = column_labels[4:6] if not deaths_labels else deaths_labels
+    plot_daily_deaths(labels, dates, numbers[:, 3:5])
+    plot_condition_vs_actives(column_labels[[4, 6, 7]], dates, numbers[:, [0, 3, 5, 6]])
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    DAILY_LABELS = ['Casos diarios', 'Activos']
+    DEATHS_LABELS = ['Fallecidos diarios', 'Acumulado']
+    plot_situation('cases.txt',
+                   daily_labels=DAILY_LABELS,
+                   deaths_labels=DEATHS_LABELS)
--- vaccines.py
+++ vaccines.py
@@ -3,7 +3,7 @@ from datetime import date
 
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.ticker import FuncFormatter, MultipleLocator, FixedLocator, AutoMinorLocator
+from matplotlib.ticker import FuncFormatter, AutoMinorLocator
 
 
 def read_data_file(filename):
@@ -11,20 +11,28 @@ def read_data_file(filename):
         labels = f.readline().strip().split('\t')
         dates, numbers = list(), list()
         for line in f.readlines():
-            if line:
+            if line.strip():
                 data = line.strip().split('\t')
                 dates.append(date.fromisoformat(data[0]))
                 numbers.append(list(map(int, data[1:])))
     return labels, dates, np.array(numbers)
 
 
-def common_style_settings(dates, n_days, y_max, y_label='', format_func=None):
+def common_style_settings(dates, n_days, y_max, y_label='', format_func=None, first_date=0):
     x_labels = [d.strftime("%d/%m") for d in dates]
     for i in range(n_days - 1):
         if i % 7:
             x_labels[n_days-1 - i] = ''
+    if first_date != 0:
+        x_labels[0] = ''
+        x_labels[first_date] = dates[first_date].strftime("%d/%m")
+        x_labels[first_date+1] = ''
+        x_labels[first_date+2] = ''
     use_labels = np.array(x_labels)
-    use_labels[[1, 2]] = ''
+    no_labels = [1, 2]
+    if first_date in no_labels:
+        no_labels.remove(first_date)
+    use_labels[no_labels] = ''
     tick_positions = np.where(x_labels)[0]
     plt.xticks(tick_positions, use_labels[tick_positions], rotation=45)
     plt.gca().set_xticks(range(n_days), minor=True)
@@ -60,9 +68,10 @@ def plot_accumulate_doses(labels, dates, numbers):
 
     n_days = len(dates)
     xs = range(n_days)
-    plt.fill_between(xs, numbers[:, 1], label=labels[2], color='tab:blue', zorder=10)
-    plt.fill_between(xs, numbers[:, 2], label=labels[3], color='tab:green', zorder=11)
-    plt.fill_between(xs, numbers[:, 3], label=labels[4], color='tab:orange', zorder=12)
+    plt.fill_between(xs, numbers[:, 1], label=labels[0], color='tab:blue', zorder=10)
+    plt.fill_between(xs, numbers[:, 2], label=labels[1], color='tab:green', zorder=11)
+    plt.fill_between(xs, numbers[:, 3], label=labels[2], color='tab:orange', zorder=12)
+    plt.fill_between(xs, numbers[:, 4], label=labels[3], color=(.88, .39, .0), zorder=11)
 
     common_style_settings(dates, n_days, numbers[:, 1].max(), 'Millones',
                           format_func=lambda x, p: f'{x*1e-6:1.1f} M' if x > 0 else '')
@@ -73,19 +82,24 @@ def plot_accumulate_doses(labels, dates, numbers):
 
 def plot_daily_doses(labels, dates, numbers):
     plt.figure('Dosis diarias', (13, 6))
-    plt.suptitle('Dosis diarias')
+    plt.suptitle(f'Dosis diarias (Últimos {len(dates) - 1} días)')
 
     n_days = len(dates)
-    xs = np.array(list(range(1, n_days)))
-    width = .29
-    colors = ['tab:blue', 'tab:green', 'tab:orange']
-    dataset = np.array([(numbers[:, i] - np.roll(numbers[:, i], 1))[1:] for i in range(4)])
-    for i in range(1, 4):
-        plt.bar(xs + width * (i-2), dataset[i], width, label=labels[i+1], zorder=100,
-                color=colors[i-1], edgecolor='k', linewidth=.5)
+    xs = np.array(list(range(1, n_days))) + .13
+    width = .24
+    colors = ['tab:blue', 'tab:green', 'tab:orange', (.88, .39, .0)]
+    dataset = np.array([(numbers[:, i] - np.roll(numbers[:, i], 1))[1:] for i in range(5)])[1:]
+    uniques = (numbers[:, -1] - numbers[:, -2])
+    uniques = (uniques - np.roll(uniques, 1))[1:]
+    dataset[0] -= uniques
+    dataset[-1] = uniques
+    for i in range(4):
+        plt.bar(xs + width * (i-2), dataset[i], width, label=labels[i], zorder=100,
+                color=colors[i], edgecolor='k', linewidth=.5)
 
-    common_style_settings(dates, n_days, dataset[0].max(), 'Miles',
-                          format_func=lambda x, p: f'{x*1e-3:1.0f} K' if x > 0 else '')
+    common_style_settings(dates, n_days, dataset.max(), 'Miles',
+                          format_func=lambda x, p: f'{x*1e-3:1.0f} K' if x > 0 else '',
+                          first_date=1)
 
 
 def plot_stacked_daily_doses(labels, dates, numbers):
@@ -95,30 +109,45 @@ def plot_stacked_daily_doses(labels, dates, numbers):
     n_days = len(dates)
     xs = np.array(list(range(1, n_days)))
     width = .86
-    dataset = np.array([(numbers[:, i] - np.roll(numbers[:, i], 1))[1:] for i in range(4)])
-    plt.bar(xs, dataset[1], width, color='tab:blue', zorder=100, label=labels[2])
-    plt.bar(xs, dataset[2], width, color='tab:green', zorder=100, label=labels[3],
-            bottom=dataset[1])
-    plt.bar(xs, dataset[3], width, color='tab:orange', zorder=100, label=labels[4],
-            bottom=dataset[1] + dataset[2])
+    dataset = np.array([(numbers[:, i] - np.roll(numbers[:, i], 1))[1:] for i in range(5)])[1:]
+    uniques = (numbers[:, -1] - numbers[:, -2])
+    uniques = (uniques - np.roll(uniques, 1))[1:]
+    dataset[0] -= uniques
+    dataset[-1] = uniques
+    plt.bar(xs, dataset[0], width, color='tab:blue', zorder=100, label=labels[0])
+    plt.bar(xs, dataset[1], width, color='tab:green', zorder=100, label=labels[1],
+            bottom=dataset[0])
+    plt.bar(xs, dataset[2], width, color='tab:orange', zorder=100, label=labels[2],
+            bottom=dataset[0] + dataset[1])
+    plt.bar(xs, dataset[3], width, color=(.88, .39, .0), zorder=100, label=labels[3],
+            bottom=dataset[0] + dataset[1] + dataset[2])
 
-    plt.bar(xs, dataset[1:].sum(axis=0), width, fill=False, edgecolor='k', linewidth=.5, zorder=200)
+    plt.bar(xs, dataset.sum(axis=0), width, fill=False, edgecolor='k', linewidth=.5, zorder=200)
     for i in range(n_days-1):
-        plt.plot([xs[i] - width / 2, xs[i] + width / 2], [dataset[1][i]] * 2, c='k', lw=.5, zorder=200)
-        plt.plot([xs[i] - width / 2, xs[i] + width / 2], [dataset[[1, 2], i].sum()] * 2, c='k', lw=.5, zorder=200)
+        plt.plot([xs[i] - width / 2, xs[i] + width / 2], [dataset[0, i]] * 2, c='k', lw=.5, zorder=200)
+        plt.plot([xs[i] - width / 2, xs[i] + width / 2], [dataset[[0, 1], i].sum()] * 2, c='k', lw=.5, zorder=200)
+        if uniques[i]:
+            plt.plot([xs[i] - width / 2, xs[i] + width / 2], [dataset[[0, 1, 2], i].sum()] * 2, c='k', lw=.5, zorder=200)
+        pass
+    common_style_settings(dates, n_days, dataset.sum(axis=0).max(), 'Miles',
+                          format_func=lambda x, p: f'{x*1e-3:1.0f} K' if x > 0 else '',
+                          first_date=1)
 
-    common_style_settings(dates, n_days, dataset[0].max(), 'Miles',
-                          format_func=lambda x, p: f'{x*1e-3:1.0f} K' if x > 0 else '')
 
-
-def plot_vaccination(filename):
-    labels, dates, numbers = read_data_file(filename)
+def plot_vaccination(filename, accum_labels=None, daily_labels=None):
+    column_labels, dates, numbers = read_data_file(filename)
+    labels = accum_labels if accum_labels else column_labels[2:]
     plot_accumulate_doses(labels, dates, numbers)
-    plot_daily_doses(labels, dates, numbers)
+    labels = daily_labels if daily_labels else column_labels[2:]
+    plot_daily_doses(labels, dates[-71:], numbers[-71:, ])
     plot_stacked_daily_doses(labels, dates, numbers)
 
     plt.show()
 
 
 if __name__ == '__main__':
-    plot_vaccination('vaccines.txt')
+    ACCUMULATED_LABELS = ['Primera dosis', 'Segunda dosis', 'Tercera dosis', 'Esquema completo']
+    DAILY_DOSES_LABELS = ['Primera dosis', 'Segunda dosis', 'Tercera dosis', 'Dosis única']
+    plot_vaccination('vaccines.txt',
+                     accum_labels=ACCUMULATED_LABELS,
+                     daily_labels=DAILY_DOSES_LABELS)
